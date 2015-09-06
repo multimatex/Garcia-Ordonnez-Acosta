0a9d84fb26d6cac98fda75be60d9304236e74b16
juanjose94 <juanjo_gato94@hotmail.com>
2015-09-06 15:49:14 -0500

    Act

---
+++ CalculadoraEstructurasDeDatos/src/calculadoraestructurasdedatos/ArrayStack.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoraestructurasdedatos;
+
+/**
+ *
+ * @author Equipo
+ */
+public class ArrayStack<E> implements Stack<E>{
+
+    private E data[];
+    private int size = 0;
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    } 
+
+    @Override
+    public E peek() {
+        
+    }
+
+    @Override
+    public E pop() {
+        
+    }
+
+    @Override
+    public void push(E recibido) {
+        
+    }
+    
+}
---
+++ CalculadoraEstructurasDeDatos/src/calculadoraestructurasdedatos/Stack.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoraestructurasdedatos;
+
+/**
+ *
+ * @author Equipo
+ */
+public interface Stack<E>
+{
+  public boolean isEmpty();
+  public E peek();
+  public E pop();
+  public void push(E recibido);
+         
+}
